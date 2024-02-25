ae98f38fb2ad39c491db0b3354196150fec80490
Cristian Banarescu <cristianbanarescu@gmail.com>
2024-02-25 21:25:08 +0200

    Created new DemoPetApp project using SwiftUI with unit and UI tests templates.

---
+++ DemoPetApp/DemoPetApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DemoPetApp
+//
+//  Created by Cristian Banarescu on 25.02.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
